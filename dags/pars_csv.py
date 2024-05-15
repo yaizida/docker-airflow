c04b3a034e3915d6ebc152b4d3bf25c537c4231e
izida <myizida@gmail.com>
2024-05-16 00:09:29 +0300

    load data into postgres

--- dags/pars_csv.py
+++ dags/pars_csv.py
@@ -1,7 +1,7 @@
 import os
 
 from airflow import DAG
-# from airflow.providers.postgres.operators.postgres import PostgresOperator
+from airflow.operators.postgres_operator import PostgresOperator
 from airflow.hooks.postgres_hook import PostgresHook
 from airflow.operators.python_operator import PythonOperator
 from airflow.operators.bash_operator import BashOperator
@@ -42,60 +42,6 @@ def extract_data(**kwargs):
     ti.xcom_push(key='load_csv_posgres', value=df)
 
 
-def into_data(**kwargs):
-    ti = kwargs['ti']
-
-    if ti is None:
-        raise ValueError('ti тут нихуя нету')
-
-    res_df = ti.xcom_pull(key='load_csv_posgres', task_ids='extract_data')
-    print(res_df)
-
-    if res_df is None:
-        raise ValueError('res_df нихуя нет')
-
-    # print(res_df.head())
-
-    #  Работа с бд
-    pg_hook = PostgresHook(postgres_conn_id='posgres_localhost')
-
-    if isinstance(pg_hook, PostgresHook):
-        print('Итс окэй')
-    else:
-        print('Иди нахуй')
-
-    # Создание таблицы в PostgreSQL, если она еще не существует
-    table_name = 'my_table'
-    create_table_query = (f"CREATE TABLE IF NOT EXISTS {table_name} " +
-                          "(year INT, industry_aggregation VARCHAR(10), " +
-                          "industry_code INT, industry_name VARCHAR(125), " +
-                          "units VARCHAR(55), varibale_code VARCHAR(125), " +
-                          "varibale_name VARCHAR(215), " +
-                          "varibale_category VARCHAR(125), " +
-                          "value DECIMAL, indastry_code VARCHAR(215));")
-
-    conn = psycopg2.connect(dbname="airflow", user="airflow",
-                            password="airflow", host="host.docker.internal")
-    cursor = conn.cursor()
-    cursor.execute(create_table_query)
-
-    # Загрузка данных из дата фрейма в таблицу PostgreSQL
-    # Эта хуйня в доработке
-    insert_query = f"""INSERT INTO {table_name}
-    (year, industry_aggregation, industry_code, industry_name,
-    units, varibale_code, varibale_name, varibale_category,
-    value, indastry_code)
-    VALUES ({r'%s, '*10});"""
-    execute_values(cursor, insert_query, res_df.to_numpy())
-
-    # Фиксация изменений в базе данных
-    conn.commit()
-
-    # Закрытие соединения с базой данных
-    cursor.close()
-    conn.close()
-
-
 with DAG('csv_to_postgres',
          default_args=default_args,
          catchup=False,
@@ -110,11 +56,28 @@ with DAG('csv_to_postgres',
         dag=dag
     )
 
-    load_data = PythonOperator(
-        task_id='load_data',
-        python_callable=into_data,
-        provide_context=True,
-        dag=dag
+    create_table = PostgresOperator(
+        task_id='create_table_value_table',
+        postgres_conn_id='database_PG',
+        sql=("CREATE TABLE IF NOT EXISTS my_table " +
+             "(year INT, industry_aggregation VARCHAR(10), " +
+             "industry_code INT, industry_name VARCHAR(125), " +
+             "units VARCHAR(55), varibale_code VARCHAR(125), " +
+             "varibale_name VARCHAR(215), " +
+             "varibale_category VARCHAR(125), " +
+             "value DECIMAL, indastry_code VARCHAR(215));")
     )
 
-    extract_csv >> load_data
+    insert_into_table = PostgresOperator(
+        task_id='insert_into_table',
+        postgres_conn_id='database_PG',
+        sql=[f"""INSERT INTO my_table VALUES(
+            {{{{ti.xcom_pull(key='load_csv_posgres', task_ids=['extract_data'])[0].iloc[{i}]['Year']}}}},
+            {{{{ti.xcom_pull(key='load_csv_posgres', task_ids=['extract_data'])[0].iloc[{i}]['Industry_aggregation']}}}},
+            {{{{ti.xcom_pull(key='load_csv_posgres', task_ids=['extract_data'])[0].iloc[{i}]['Industry_code']}}}},
+            {{{{ti.xcom_pull(key='load_csv_posgres', task_ids=['extract_data'])[0].iloc[{i}]['Industry_name']}}}},
+            )""" for i in range(5)]
+
+    )
+
+    extract_csv >> create_table >> insert_into_table
